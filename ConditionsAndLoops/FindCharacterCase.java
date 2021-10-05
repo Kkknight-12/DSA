a96ce0e24f2e0edf3ae823f74c652499462a3780
Knight <mayanks365@gmail.com>
2021-10-05 11:09:28 +0530

    checking inteliji

--- ConditionsAndLoops/FindCharacterCase.java
+++ ConditionsAndLoops/FindCharacterCase.java
@@ -5,8 +5,6 @@ import java.util.Scanner;
 public class FindCharacterCase {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-
         char ch = input.next().charAt(0);
         if(ch>=65 && ch<=90)
             System.out.println(1);
