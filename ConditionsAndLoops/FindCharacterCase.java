b99d1873a54c5049645118004662d261ae54260e
Knight <mayanks365@gmail.com>
2021-10-05 11:15:53 +0530

    adding comments

--- ConditionsAndLoops/FindCharacterCase.java
+++ ConditionsAndLoops/FindCharacterCase.java
@@ -4,8 +4,14 @@ import java.util.Scanner;
 
 public class FindCharacterCase {
     public static void main(String[] args) {
+
+        // scanner
         Scanner input = new Scanner(System.in);
+
+        // taking the first letter from the input to check
         char ch = input.next().charAt(0);
+
+        // conditional loop
         if(ch>=65 && ch<=90)
             System.out.println(1);
         else if(ch>=97 && ch<=122)
